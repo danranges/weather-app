193cf415c5f2f325bbb906844da8293d199986f9
Dan Ranges <danranges@gmail.com>
2021-12-21 21:53:45 +0100

    Add icon, precipitation, hi/low temps to Daily Forecast

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -1,12 +1,39 @@
 import { format, isToday, toDate } from 'date-fns';
+import { hi } from 'date-fns/locale';
+import Storage from '../Storage';
 
 const singleDayForecast = (day) => {
-  const forecast = document.createElement('div');
-  forecast.classList.add('flex', 'row');
+  const { metric } = Storage.getSettings();
+  const tempMax = Math.round(metric ? day.temp.max : day.temp.max * 1.8 + 32);
+  const tempMin = Math.round(metric ? day.temp.min : day.temp.min * 1.8 + 32);
 
-  const dayName = document.createElement('p');
+  const forecast = document.createElement('div');
+  forecast.classList.add('align-center', 'single-day-wrapper');
+
+  const dayName = document.createElement('div');
   dayName.textContent = format(toDate(day.dt * 1000), 'eeee');
   forecast.appendChild(dayName);
+
+  const weatherIcon = new Image();
+  weatherIcon.classList.add('hourly-icon');
+  weatherIcon.src = `http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png`;
+  forecast.appendChild(weatherIcon);
+
+  const precipitationChance = document.createElement('div');
+  precipitationChance.classList.add('bold', 'ht-1');
+  precipitationChance.textContent = day.pop === 0 ? '' : `${Math.round(day.pop * 100)}%`;
+  forecast.appendChild(precipitationChance);
+
+  const hiLoWrapper = document.createElement('div');
+  hiLoWrapper.classList.add('flex', 'wrapper', 'temps');
+  const highTemp = document.createElement('p');
+  highTemp.textContent = `H: ${tempMax}°`;
+  const lowTemp = document.createElement('p');
+  lowTemp.textContent = `L: ${tempMin}°`;
+  hiLoWrapper.appendChild(highTemp);
+  hiLoWrapper.appendChild(lowTemp);
+  forecast.appendChild(hiLoWrapper);
+
   return forecast;
 };
 
